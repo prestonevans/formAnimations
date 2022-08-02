595b235204003bb5e2a5b143da59fab07cde58c9
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-08-02 12:56:07 -0600

    Made my own material design inputs

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  input.addEventListener('change', (e) => {
+    const label = e.target.nextElementSibling;
+    if (e.target.value.length > 0) {
+      label.classList.add('transform');
+      return;
+    }
+    label.classList.remove('transform');
+  });
+});
